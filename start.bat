5e313a44fec922b2e3c7c67cbbd73b9119bbf092
david419kr <david419kr@gmail.com>
2024-06-23 11:04:44 +0900

    add venv and requirements.txt

---
+++ start.bat
@@ -0,0 +1,26 @@
+@echo off
+set "VENV_DIR=%~dp0venv"
+set "CLIENT_SCRIPT=client.py"
+
+if exist "%VENV_DIR%\Scripts\activate.bat" (
+    echo Virtual environment already exists. Activating it...
+    call "%VENV_DIR%\Scripts\activate.bat"
+
+    echo Running streamlit application...
+    streamlit run "%~dp0%CLIENT_SCRIPT%"
+) else (
+    echo Virtual environment does not exist. Creating a new one...
+
+    echo Creating virtual environment in %VENV_DIR%
+    python -m venv "%VENV_DIR%
+    call "%VENV_DIR%\Scripts\activate.bat"
+
+    echo Installing required packages...
+    pip install -r requirements.txt
+
+    echo Running streamlit application...
+    streamlit run "%~dp0%CLIENT_SCRIPT%"
+)
+
+echo Done.
+pause
